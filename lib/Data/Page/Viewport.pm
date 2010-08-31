c7a898b9770ff722f6c6eef9afec74893de35d82
Ron Savage <ron@savage.net.au>
2010-08-31 07:25:31 +0000

    Import of RSAVAGE/Data-Page-Viewport-1.06 from CPAN.
    gitpan-cpan-distribution: Data-Page-Viewport
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         RSAVAGE/Data-Page-Viewport-1.06.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/Data/Page/Viewport.pm
+++ lib/Data/Page/Viewport.pm
@@ -56,7 +56,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 
 );
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 # -----------------------------------------------
 
@@ -450,9 +450,9 @@ C<Data::Page::Viewport> was written by Ron Savage I<E<lt>ron@savage.net.auE<gt>>
 
 Home page: http://savage.net.au/index.html
 
-=head1 Copybottom
+=head1 Copyright
 
-Australian copybottom (c) 2004, Ron Savage. All bottoms reserved.
+Australian copyright (c) 2004, Ron Savage.
 
 	All Programs of mine are 'OSI Certified Open Source Software';
 	you can redistribute them and/or modify them under the terms of
